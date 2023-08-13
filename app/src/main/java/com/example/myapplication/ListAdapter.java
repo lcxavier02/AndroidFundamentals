2a6418eaee01af9b854ba5d8dbde20a37b2937a8
lcxavier02 <luisvelazquez98271@gmail.com>
2023-08-13 14:03:09 -0600

    Adding RecyclerView logic

--- app/src/main/java/com/example/myapplication/ListAdapter.java
+++ app/src/main/java/com/example/myapplication/ListAdapter.java
@@ -1,9 +1,13 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -33,6 +37,28 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(final ListAdapter.ViewHolder holder, final int position) {
         holder.bindData(mData.get(position));
+    }
+
+    public void setItems(List<ListElement> items) { mData = items; }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView iconImage;
+        TextView name, city, status;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            iconImage = itemView.findViewById(R.id.iconImageView);
+            name = itemView.findViewById(R.id.nameTextView);
+            city = itemView.findViewById(R.id.cityTextView);
+            status = itemView.findViewById(R.id.statusTextView);
+        }
+
+        void bindData(final ListElement item) {
+            iconImage.setColorFilter(Color.parseColor(item.getColor()), PorterDuff.Mode.SRC_IN);
+            name.setText(item.getName());
+            city.setText(item.getCity());
+            status.setText(item.getStatus());
+        }
     }
 
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<ListElement> elements;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    public void init() {
+        elements = new ArrayList<>();
+        elements.add(new ListElement("#922B21", "John Doe", "USA", "Active"));
+        elements.add(new ListElement("#8E44AD", "Aarika", "Afghanistan", "Inactive"));
+        elements.add(new ListElement("#FCF3CF", "Briana", "Barbados", "Active"));
+        elements.add(new ListElement("#E67E22", "Chrystel", "Germany", "Active"));
+        elements.add(new ListElement("#48C9B0", "Cinderella", "India", "Dead"));
+        elements.add(new ListElement("#5D6D7E", "Helga", "Honduras", "Cancelled"));
+        elements.add(new ListElement("#0E6655", "Gust", "Morocco", "Active"));
+        elements.add(new ListElement("#784212", "Jorge", "Mexico", "Active"));
+        elements.add(new ListElement("#81D4FA", "Lemmy", "Zambia", "Inactive"));
+        elements.add(new ListElement("#AED581", "Natanael", "Japan", "Active"));
+
+        ListAdapter listAdapter = new ListAdapter(elements, this);
+        RecyclerView recyclerView = findViewById(R.id.listRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(listAdapter);
     }
 }
