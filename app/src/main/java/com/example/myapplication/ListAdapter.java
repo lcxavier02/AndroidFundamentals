ab81138dd9e064273a468307349d22d81c56848c
lcxavier02 <luisvelazquez98271@gmail.com>
2023-08-13 13:31:36 -0600

    Initial files

---
+++ app/src/main/java/com/example/myapplication/ListAdapter.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
+
+    private List<ListElement> mData;
+    private LayoutInflater mInflater;
+    private Context context;
+
+    public ListAdapter(List<ListElement> itemList, Context context) {
+        this.mInflater = LayoutInflater.from(context);
+        this.context = context;
+        this.mData = itemList;
+    }
+
+    @Override
+    public int getItemCount() { return mData.size(); }
+
+    @Override
+    public ListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.list_element, null);
+        return new ListAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ListAdapter.ViewHolder holder, final int position) {
+        holder.bindData(mData.get(position));
+    }
+
+}
