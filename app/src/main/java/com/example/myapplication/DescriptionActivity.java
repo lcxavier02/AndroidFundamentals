a3762de313abf2194589aa79371f8021005ad5be
lcxavier02 <luisvelazquez98271@gmail.com>
2023-08-13 22:33:39 -0600

    Creating custom toasts

--- app/src/main/java/com/example/myapplication/DescriptionActivity.java
+++ app/src/main/java/com/example/myapplication/DescriptionActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import io.github.muddz.styleabletoast.StyleableToast;
 
 public class DescriptionActivity extends AppCompatActivity {
 
     TextView titleDescriptionTextView;
     TextView cityDescriptionTextView;
     TextView statusDescriptionTextView;
+
+    Button buttonToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,7 @@ public class DescriptionActivity extends AppCompatActivity {
         titleDescriptionTextView = findViewById(R.id.titleDescriptionTextView);
         cityDescriptionTextView = findViewById(R.id.cityDescriptionTextView);
         statusDescriptionTextView = findViewById(R.id.statusDescriptionTextView);
+        buttonToast = findViewById(R.id.btnShowToast);
 
         titleDescriptionTextView.setText(element.getName());
         titleDescriptionTextView.setTextColor(Color.parseColor(element.getColor()));
@@ -29,5 +36,14 @@ public class DescriptionActivity extends AppCompatActivity {
 
         statusDescriptionTextView.setText(element.getStatus());
         statusDescriptionTextView.setTextColor(Color.BLUE);
+
+        buttonToast.setBackgroundColor(Color.parseColor(element.getColor()));
+
+        buttonToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StyleableToast.makeText(DescriptionActivity.this, element.getName()+" Toast!", R.style.exampleToast).show();
+            }
+        });
     }
 }
