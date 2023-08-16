372b1ed9b89b458343e7cad6ee9e9caee2bd3ac7
lcxavier02 <luisvelazquez98271@gmail.com>
2023-08-15 22:07:21 -0600

    Added EditText and Switch functionalities

--- app/src/main/java/com/example/myapplication/fragments/TextsFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/TextsFragment.java
@@ -7,14 +7,41 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class TextsFragment extends Fragment {
+
+    TextInputLayout textInputLayout;
+    TextInputEditText textInputEditText;
+    TextView textView;
+    Button button;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_texts, container, false);
+        View view = inflater.inflate(R.layout.fragment_texts, container, false);
+
+        textInputLayout = view.findViewById(R.id.textInputLayout);
+        textInputEditText = view.findViewById(R.id.textInputEditText);
+        textView = view.findViewById(R.id.textView);
+        button = view.findViewById(R.id.buttonEditText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateText();
+            }
+        });
+
+        return view;
+    }
+
+    public void updateText() {
+        textView.setText("You wrote: " + textInputEditText.getText());
     }
 }
--- app/src/main/java/com/example/myapplication/fragments/ToggleFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/ToggleFragment.java
@@ -7,14 +7,35 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
 
 public class ToggleFragment extends Fragment {
+    Switch aSwitch;
+    TextView textView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_toggle, container, false);
+        View view = inflater.inflate(R.layout.fragment_toggle, container, false);
+
+        aSwitch = view.findViewById(R.id.toggleSwitch);
+        textView = view.findViewById(R.id.textView);
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    textView.setText("Now is activated");
+                } else {
+                    textView.setText("But now is not activated");
+                }
+            }
+        });
+
+        return view;
     }
 }
